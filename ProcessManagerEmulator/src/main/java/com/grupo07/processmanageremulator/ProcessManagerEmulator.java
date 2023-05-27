bb59160a03fd7e77f6b25f125610cf677910123a
Diego Jimenez <diegojmoravia@gmail.com>
2023-05-27 16:25:12 -0600

    Initial structure of main menu

---
+++ ProcessManagerEmulator/src/main/java/com/grupo07/processmanageremulator/ProcessManagerEmulator.java
@@ -0,0 +1,20 @@
+
+package com.grupo07.processmanageremulator;
+
+import controllers.MainMenuController;
+import models.MainMenuModel;
+import views.MainMenuView;
+
+
+public class ProcessManagerEmulator {
+
+    public static void main(String[] args) {
+  
+       MainMenuView mainView = new MainMenuView();
+       MainMenuModel mainMenuModel = new MainMenuModel();
+       MainMenuController mainMenuController = new MainMenuController(mainView,mainMenuModel);
+       mainView.setVisible(true);
+    }
+    
+   
+}
---
+++ ProcessManagerEmulator/src/main/java/models/MainMenuModel.java
@@ -0,0 +1,20 @@
+
+package models;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class MainMenuModel extends Observable  {
+    
+    @Override
+    public void addObserver(Observer o){
+        super.addObserver(o);
+        refresh();
+    }
+    
+     private void refresh(){
+        setChanged();
+        notifyObservers();
+    }
+    
+}
